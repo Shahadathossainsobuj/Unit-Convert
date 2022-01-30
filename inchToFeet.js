1f328d37e9792bfaa7e625cbafb414656fc1a004
Shahadathossainsobuj <sobuj39@diit.info>
2022-01-30 21:13:58 +0600

    first commit

---
+++ inchToFeet.js
@@ -0,0 +1,18 @@
+function inchToFeet(inches) {
+    var feet = inches / 12;
+    return feet;
+}
+inches = 144;
+var feet = inchToFeet(inches);
+console.log(feet);
+
+
+
+
+function inchtofeet(inches) {
+    var feet = inches / 12;
+    return feet;
+}
+var inches = 240;
+var feet = inchtofeet(inches);
+console.log(feet);
